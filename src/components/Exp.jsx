215453cb80acc6863035d231e779fb40785f55f8
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2022-09-18 22:14:38 +0530

    fixed type

--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -12,13 +12,10 @@ const Exp = () => {
         <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
           <div>
             <p className=" text-4xl font-bold inline border-b-4 border-pink-600">
-              Expirience
+              Experience
             </p>
           </div>
           <VerticalTimeline className="-z-0 md:z-0">
-
-
-
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
               contentStyle={{
